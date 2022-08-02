6d9c47ee8567bde8ed56bd2e4ce040b89688a5e8
qgtere <qg.tere@gmail.com>
2022-08-02 17:47:48 -0600

    Initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+
+var formEl = document.querySelector("#movieForm");
+var movieNameEl = document.querySelector("#movieName");
+
+const movieFormSubmit = function(event){
+    event.preventDefault();
+    
+    let movie = (movieNameEl.value.trim() + " review").replaceAll(" ", "%20");
+    console.log(movie);
+
+    if (movie != "") {
+        window.location.assign("review-page.html?movie=" + movie);
+    } else {
+        movieNameEl.value = '';
+        alert("You must enter a Movie Name");
+        movieNameEl.value = '';
+        movieNameEl.focus();
+    }
+
+};
+
+formEl.addEventListener("submit", movieFormSubmit);
